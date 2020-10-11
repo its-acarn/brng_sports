cfc1d17c4a764cd0726b82aabb8c7c7bb6cefd28
svvishjs <andreww92@live.com>
2020-10-11 14:14:35 +0100

    added database_url in run_sql file

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,12 +1,13 @@
-import psycopg2  
+import psycopg2, os  
 import psycopg2.extras as ext
 
 def run_sql(sql, values = None):
     conn = None
     results = []
-    
+    database_url = os.getenv('DATABASE_URL')
+
     try:
-        conn=psycopg2.connect("dbname='postgres://usxgbeocilcais:bb23b554c2d5002767e60615684e43912b893104e91fe96f84875210c7cec8ad@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/df47ikncj3r9tc'")
+        conn=psycopg2.connect(database_url)
         cur = conn.cursor(cursor_factory=ext.DictCursor)   
         cur.execute(sql, values)
         conn.commit()
